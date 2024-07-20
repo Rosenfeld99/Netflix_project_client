294807a32e9b7e620bf37e506a13fda721189bac
Rosenfeld99 <124917070+Rosenfeld99@users.noreply.github.com>
2024-07-21 00:41:59 +0300

    deploy with new footer

--- src/components/components/footer/Footer.jsx
+++ src/components/components/footer/Footer.jsx
@@ -1,6 +1,9 @@
 import React from "react";
-import { BsFacebook, BsGithub, BsLinkedin } from "react-icons/bs";
+import { FaUserCircle } from "react-icons/fa";
+import { BsGithub } from "react-icons/bs";
+import { BsLinkedin } from "react-icons/bs";
 import { Link } from "react-router-dom";
+
 
 const Footer = () => {
   const getYear = () => {
@@ -10,7 +13,7 @@ const Footer = () => {
 
   const currentYear = getYear()
   return (
-    <footer className="footer footer-center p-10 rounded">
+    <footer className="footer footer-center p-10 pt-0 rounded">
       {/* <div className="grid grid-flow-col gap-4">
         <a className="link link-hover">About us</a>
         <a className="link link-hover">Contact</a>
@@ -19,20 +22,20 @@ const Footer = () => {
       </div> */}
       <div>
         <div className="grid grid-flow-col gap-4">
-          <Link to={"https://www.linkedin.com/in/eli-rosenfeld-3245511b2/"}>
+          <a className="hover:text-blue-500" target="_blank" rel="noopener noreferrer" href={"https://www.linkedin.com/in/eli-rosenfeld"}>
             <BsLinkedin className=" text-xl" />
-          </Link>
-          <Link to={"https://github.com/Rosenfeld99"}>
-            <BsGithub className=" text-xl" />
-          </Link>
-          <Link to={"#"}>
-            <BsFacebook className=" text-xl" />
-          </Link>
+          </a>
+          <a className="hover:text-blue-500" target="_blank" rel="noopener noreferrer" href={"https://github.com/Rosenfeld99"}>
+            <BsGithub className=" text-2xl" />
+          </a>
+          <a className="hover:text-blue-500" target="_blank" rel="noopener noreferrer" href={"https://eli-rosenfeld-protfolio.netlify.app/"}>
+            <FaUserCircle className=" text-2xl" />
+          </a>
         </div>
       </div>
       <div>
-        <p>Copyright © {currentYear} - Eli rosenfeld</p>
-        <p>Fullstack Developer | MongoDB | Node.js</p>
+        <p>Copyright © {currentYear} - <a className="link link-hover hover:text-blue-500" target="_blank" rel="noopener noreferrer" href={"https://eli-rosenfeld-protfolio.netlify.app/"}> Eli Rosenfeld </a></p>
+        {/* <p>Fullstack Developer | MongoDB | Node.js</p> */}
       </div>
     </footer>
   );
